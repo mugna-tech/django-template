c0026c57cfff99c51c0211f2b16aacf6831cf799
Lorence Lim <jlorencelim@gmail.com>
2022-12-06 15:56:12 +0800

    Update lock and add users in admin

--- {{cookiecutter.project_slug}}/users/admin.py
+++ {{cookiecutter.project_slug}}/users/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from users.models import User
+
+
+@admin.register(User)
+class UserAdmin(UserAdmin):
+    pass
